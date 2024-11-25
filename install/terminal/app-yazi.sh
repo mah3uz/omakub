1020456ac9c561cffb422c3ade2ced60348ae5f7
Mahfuz Shaikh <mah3uz@gmail.com>
2024-11-25 19:26:56 +0600

    Removed yazi

--- install/terminal/app-yazi.sh
+++
@@ -1,8 +0,0 @@
-# Best terminal filemanager
-# https://yazi-rs.github.io/docs/installation
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup update
-
-if [ ! -d "$HOME/.config/yazi" ]; then
-  cp -r ~/.local/share/omakub/configs/yazi ~/.config
-fi
