c257cad1c46fab6301d6263539844c7b52f6465c
Mahfuz Shaikh <mah3uz@gmail.com>
2024-12-04 12:44:44 +0600

    Checked if rust installed

--- install/terminal.sh
+++ install/terminal.sh
@@ -3,8 +3,12 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y curl git unzip
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-. "$HOME/.cargo/env"
+if command -v cargo &> /dev/null; then
+    echo "Rust and Cargo already installed"
+else
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    . "$HOME/.cargo/env"
+fi
 
 # Run terminal installers
 for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
